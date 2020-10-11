f070f4be59cb300edc74899149db16068e83f45b
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2020-10-11 12:53:24 +0200

    Fix naming

---
+++ src/components/ListFooter/ListFooter.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ListFooter from './index';
+
+export default {
+  title: 'ListFooter',
+  component: ListFooter,
+};
+
+const Template = (args) => <ListFooter {...args} />;
+
+export const Default = Template.bind({});
--- src/components/ListFooter/index.jsx
+++ src/components/ListFooter/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Plus } from '@styled-icons/boxicons-regular/Plus';
-const ListHeader = () => {
+const ListFooter = () => {
   return (
-    <ListHeaderWrapper>
+    <ListFooterWrapper>
       <PlusIcon size={20} />
       <span>Add another card</span>
-    </ListHeaderWrapper>
+    </ListFooterWrapper>
   );
 };
 
@@ -15,14 +15,14 @@ const PlusIcon = styled(Plus)`
   margin-right: 5px;
 `;
 
-const ListHeaderWrapper = styled.div`
+const ListFooterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  border-radius: 5px;
   &:hover {
     background-color: rgba(9, 30, 66, 0.08);
   }
-  padding: 5px;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
 `;
 
-export default ListHeader;
+export default ListFooter;
