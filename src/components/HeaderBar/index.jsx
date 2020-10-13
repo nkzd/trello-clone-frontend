5229f116d8acb2c24026049f0ccc7a6ed5cdedb4
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2020-10-13 23:00:42 +0200

    Add Options icon

--- src/components/HeaderBar/index.jsx
+++ src/components/HeaderBar/index.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import { DotsHorizontalRounded } from '@styled-icons/boxicons-regular/DotsHorizontalRounded';
+
 const HeaderBar = () => {
   return (
     <HeaderBarWrapper>
-      Whateverello
+      <div>Whateverello</div>
+      <DotsIcon size={25} />
     </HeaderBarWrapper>
   );
 };
 
+const DotsIcon = styled(DotsHorizontalRounded)`
+display: inline-block;
+  color: white;
+`;
+
 const HeaderBarWrapper = styled.div`
   margin: 0 auto;
-  background-color: rgba(0,0,0,.32);
+  background-color: rgba(0, 0, 0, 0.32);
   font-family: 'Dancing Script', cursive;
-  text-align: center;
   padding: 5px;
   font-size: 2em;
-  color: #FAFAFA;
+  color: #fafafa;
+
+    display: grid; 
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+
+    div {
+        grid-column-start: 2;
+    }
+
+    ${DotsIcon} {
+        margin-left: auto;
+        &:hover {
+            color: #616161;
+        }
+    }
 `;
 
 export default HeaderBar;
