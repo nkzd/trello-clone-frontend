da34982289ec6a26747e13a573049e98979aaf19
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2020-11-01 18:36:15 +0100

    Add AddButton & XIcon

---
+++ src/components/XButton/XButton.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import XButton from './index';
+
+export default {
+  title: 'XButton',
+  component: XButton,
+};
+
+const Template = (args) => <XButton {...args} />;
+
+export const Default = Template.bind({});
+
---
+++ src/components/XButton/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import { X } from '@styled-icons/boxicons-regular/X';
+
+const XButton = styled(X)`
+  width: 20px;
+  height: 20px;
+  color: #42526e;
+  &:hover {
+    color: #172b4d;
+  }
+`;
+
+export default XButton;
