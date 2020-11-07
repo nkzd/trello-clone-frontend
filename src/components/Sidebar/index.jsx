aa0d9b32432c15dfdbe19da8c76034d7d37a8a34
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2020-11-07 15:04:27 +0100

    Add solid colors

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,10 +5,17 @@ const Sidebar = () => {
   return (
     <SidebarWrapper>
       <SidebarHeader>
-        <span className="flexAlign" />
+        <span className='flexAlign' />
         <span>Background</span>
         <XButton />
       </SidebarHeader>
+      <hr />
+      <SidebarBody>
+        <SolidColorWrapper color='rgb(131, 140, 145)' />
+        <SolidColorWrapper color='rgb(0, 121, 191);' />
+        <SolidColorWrapper color='rgb(176, 70, 50)' />
+        <SolidColorWrapper color='rgb(75, 191, 107)' />
+      </SidebarBody>
     </SidebarWrapper>
   );
 };
@@ -29,8 +36,28 @@ const SidebarHeader = styled.div`
   .flexAlign {
     display: inline-block;
     width: 26px;
-    height:26px;
+    height: 26px;
   }
+`;
+
+const SidebarBody = styled.div`
+  width: 340px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+  height: 100vh;
+`;
+
+const SolidColorWrapper = styled.div`
+  box-sizing: border-box;
+  width: 152px;
+  height: 96px;
+  border-radius: 5px;
+  background-color: ${(props) => props.color};
+  margin: 5px;
 `;
 
 export default Sidebar;
