d38f776f59282b51ddc973b7abfcc8a21d406991
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2021-01-03 12:42:01 +0100

    Add another card component

--- src/components/ListFooter/index.jsx
+++ src/components/ListFooter/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Plus } from '@styled-icons/boxicons-regular/Plus';
+import { useState } from 'react';
+import AddButton from '../AddButton';
+import XButton from '../XButton';
+
 const ListFooter = () => {
-  return (
-    <ListFooterWrapper>
+  const [enableForm, setEnableForm] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleCardClick = () => {
+    setEnableForm(!enableForm);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {};
+
+  return enableForm ? (
+    <FormWrapper>
+      <form>
+        <StyledInput
+          type='text'
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <div>
+          <AddButton text='Add Card' onClick={handleSubmit} />
+          <XButton onClick={handleCardClick} />
+        </div>
+      </form>
+    </FormWrapper>
+  ) : (
+    <ListFooterWrapper onClick={handleCardClick}>
       <PlusIcon size={20} />
       <span>Add another card</span>
     </ListFooterWrapper>
@@ -15,11 +46,30 @@ const PlusIcon = styled(Plus)`
   margin-right: 5px;
 `;
 
+const FormWrapper = styled.div`
+  width: 262px;
+  background-color: #ebecf0;
+  padding: 5px;
+  border-radius: 5px;
+  ${XButton} {
+    margin-left: 5px;
+  }
+`;
+
+const StyledInput = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  line-height: 20px;
+  padding: 6px 12px;
+  margin-bottom: 5px;
+`;
+
 const ListFooterWrapper = styled.div`
   display: flex;
   align-items: center;
   padding: 5px;
   border-radius: 5px;
+  margin-bottom: 8px;
   cursor: pointer;
   &:hover {
     background-color: rgba(9, 30, 66, 0.08);
