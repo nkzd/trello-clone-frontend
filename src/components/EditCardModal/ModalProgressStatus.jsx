f1cc20a450db126c03f42739a1a80c652d4a2d02
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2021-01-06 12:26:26 +0100

    Add ModalProgressStatus

---
+++ src/components/EditCardModal/ModalProgressStatus.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import ProgressStatus from '../../components/ProgressStatus';
+const ModalProgressStatus = ({ progressStatus }) => {
+  return (
+    <ModalProgressStatusWrapper>
+      <p>PROGRESS STATUS</p>
+      <ProgressStatusWrapper>
+        <span>{progressStatus}</span>
+        <ProgressStatus progressStatus={progressStatus} />
+      </ProgressStatusWrapper>
+    </ModalProgressStatusWrapper>
+  );
+};
+
+const ModalProgressStatusWrapper = styled.div`
+  p { 
+    color: #5e6c84;
+    font-size: 12px;
+  }
+`;
+const ProgressStatusWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  span:first-child {
+    margin-right: 5px;
+  }
+`;
+
+export default ModalProgressStatus;
