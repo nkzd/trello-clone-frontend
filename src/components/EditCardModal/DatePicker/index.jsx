5d51c12263c316ba229212233b85352d1f5069b0
Aljosa Vukotic <aljosa.vukotic@gmail.com>
2021-01-21 23:49:31 +0100

    Implement DueDate crud

--- src/components/EditCardModal/DatePicker/index.jsx
+++ src/components/EditCardModal/DatePicker/index.jsx
@@ -4,23 +4,26 @@ import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import styled from 'styled-components';
 
-const DatePicker = ({closePopup}) => {
+const DatePicker = ({ closePopup, patchChanges }) => {
   const [selectedDay, setSelectedDay] = useState(undefined);
 
   //tipa date
   //na klik kroz reducer dodaj u karticu duedate
   const handleDayClick = (day) => {
     setSelectedDay(day);
+    patchChanges({ dueDate: day.getTime() });
     closePopup();
   };
 
-  return <StyledDayPicker onDayClick={handleDayClick} selectedDays={selectedDay} />;
+  return (
+    <StyledDayPicker onDayClick={handleDayClick} selectedDays={selectedDay} />
+  );
 };
 
 const StyledDayPicker = styled(DayPicker)`
   .DayPicker-wrapper {
     background-color: white;
-    border: 1px solid #E8E8E8;
+    border: 1px solid #e8e8e8;
   }
 `;
 
